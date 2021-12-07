422d156e7b2699ab938e86c99348dbb86055f5bd
Md. Aminul Islam <aminulislammd97@gmail.com>
2021-12-07 11:16:58 +0600

    Payment System seeder added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,8 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         $this->call([
             UserSeeder::class,
-            BankSettingSeeder::class
+            BankSettingSeeder::class,
+            PaymentSystemSeeder::class
         ]);
     }
 }
